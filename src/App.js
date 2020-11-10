ebb549afb6eb8533c43773ab052fd1314ce66fe8
sm185402 <sm185402@ncr.com>
2020-11-10 20:10:53 +0530

    feat: basic initial commit.

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  // const str = "101";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to refresh.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
